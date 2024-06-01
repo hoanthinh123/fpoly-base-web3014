c3c222a9ac729e029d5e721fab0bdf316e423a17
hoan <hoanthinh311@gmail.com>
2024-06-01 08:48:28 +0700

    update

---
+++ src/Controllers/Client/HomeController.php
@@ -0,0 +1,16 @@
+<?php 
+
+namespace Asus\FpolyBaseWeb3014\Controllers\Client;
+
+use Asus\FpolyBaseWeb3014\Commons\Controller;
+
+class HomeController extends Controller
+{
+    public function index() {
+        $name = 'Hoanntph45196';
+
+        $this->renderViewClient('home', [
+            'name' => $name
+        ]);
+    }
+}
